52db791a6beff1c6851bde62b842b9a5f0c7d088
Mohamed Wael <126605463+MohamedWael257@users.noreply.github.com>
2024-09-04 22:15:05 +0300

    Delete src/redux directory

--- src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import jobslice from './slice/jobslice';
-const rootreducer = combineReducers({
-    jobs: jobslice.reducer,
-});
-const store = configureStore({
-    reducer: rootreducer
-})
-export default store;
