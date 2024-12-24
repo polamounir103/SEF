80ed67f2474b4196cfb52345148ca635b2c0b5de
polamounir103 <polamounir103@gmail.com>
2024-12-24 18:11:47 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -173,7 +173,7 @@ function Header() {
           </ul>
         </div>
         {/*Testing The Protected Routes */}
-        <div>
+        {/* <div>
           <Link className="btn text-light" to="/instructorportal">
             Instructor Portal
           </Link>
@@ -183,7 +183,7 @@ function Header() {
           <Link className="btn text-light" to="/adminportal">
             Admin Portal
           </Link>
-        </div>
+        </div> */}
         {/* Main Navigation */}
         <div className="d-flex">
           <ul className="d-flex align-items-center list-unstyled gap-3">
